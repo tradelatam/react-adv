04de59f81342b3653432e777859c52a642fb105a
Gerardo Huamanciza <ghuamanciza@gmail.com>
2022-03-01 11:01:41 -0500

    clear styles and shpage

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -12,8 +12,7 @@ export const ShoppingPage = () => {
         <hr />
 
         <ProductCard
-            product={ product }
-            className="bg-dark text-white"
+            product={ product }            
             initialValues={{
                 count:4,
                 maxCount:12
@@ -22,17 +21,9 @@ export const ShoppingPage = () => {
             {
                 ( args ) => (
                     <>
-                        <ProductImage className="custom-image" style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }}/>
-                        <ProductTitle className="text-white" />
-                        <ProductButtons className="custom-buttons"/>
-                        
-                        <button onClick={ args.reset }> Reset </button>
-                        <button onClick={ ()=>args.increaseBy(-2) }> -2 </button>
-                        {
-                            (!args.isMaxCountReached && <button onClick={ ()=>args.increaseBy(2) }> +2 </button>)
-                        }                        
-                        <span>count: {args.count} - {args.maxCount}</span>
-                        {/* { JSON.stringify( args, null, 3 )} */}
+                        <ProductImage />
+                        <ProductTitle />
+                        <ProductButtons />
                     </>
                 ) 
             }
